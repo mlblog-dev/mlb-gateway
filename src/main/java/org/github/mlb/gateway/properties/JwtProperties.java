9e7641b04f286e3f71e408fad3d9c8e2c97fac66
JiHongYuan <1206429202@qq.com>
2022-12-18 17:46:17 +0800

    Fix 依赖

--- src/main/java/org/github/mlb/gateway/properties/JwtProperties.java
+++ src/main/java/org/github/mlb/gateway/properties/JwtProperties.java
@@ -1,7 +1,5 @@
 package org.github.mlb.gateway.properties;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -9,8 +7,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author JiHongYuan
  * @date 2022/3/7 15:10
  */
-@Getter
-@Setter
 @Configurable
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
